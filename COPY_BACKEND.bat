d4017af0032413ed2ce4f0f17136edef017e7d53
hhc <2213412717@qq.com>
2026-01-26 12:56:18 +0800

    Add .gitignore and remove .vscode and .claude folders

--- COPY_BACKEND.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo 正在复制backend目录到ASD_Agent项目...
-xcopy /E /I /Y "e:\TencentRecord\xwechat_files\wxid_q3omonsd512m12_8f6b\msg\file\2026-01\backend" "e:\PythonProject\ASD_Agent\backend"
-echo.
-echo 复制完成！
-echo 现在可以将ASD_Agent文件夹拖拽到魔搭创空间部署了。
-pause
